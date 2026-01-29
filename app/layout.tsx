af9e2c61f6c12fa83603a53100a724a3a8b047ba
unknown <adsioghfdstrgsyh@gmail.com>
2026-01-29 12:30:33 +0530

    fixed google verification and seo issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export const metadata: Metadata = {
 
   // Search Console Verification (Bad porechilo)
   verification: {
-    google: 'your-google-verification-code', // Google Search Console code ekhane bosaben
+    google: 'content="h-hbJRbdNA3r3d8zWq6wjJ5RFKnwNkkjfcEjXF74Ld0', // Google Search Console code ekhane bosaben
   },
 };
 
